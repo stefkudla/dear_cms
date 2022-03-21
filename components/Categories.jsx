57624e374f3d07ebc0ec407d3d8670d8ec92779e
stefkudla <stefankudla@gmail.com>
2022-03-21 14:05:44 -0700

    Added Recent Posts and Categories Widgets

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+
+import { getCategories } from '../services'
 
 const Categories = () => {
-  return <div>Categories</div>
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+      {categories.map((category) => (
+        <Link key={category.slug} href={`/category/${category.slug}`}>
+          <span className="mb-3 block w-fit cursor-pointer rounded p-1 transition hover:bg-green-500 hover:text-white">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default Categories
